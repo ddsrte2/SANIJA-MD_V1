116f928c0f521113ad27e85f63ebcf959ba2ec8b
ddsrte2 <egsuhshd@gmail.com>
2025-03-02 20:23:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.fm/u/pz4ws8d4px",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello,I am SANIJA-MD_V2. I am Alive Now!✅   🔰My youtube channel :-https://youtube.com/@futuretechwithsanija?si=5nlmeymqJOXU1599  ",
 };
